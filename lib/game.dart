6cf4b491126fb8165223ef9f25faea7509145109
hoon <wlgnsdi@paran.com>
2025-07-04 13:33:34 +0900

    add: 설명 추가

--- lib/game.dart
+++ lib/game.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 
 import 'package:monster_battle_console/model/character.dart';
 import 'package:monster_battle_console/model/monster.dart';
-import 'package:monster_battle_console/utill/file_manager.dart';
+import 'package:monster_battle_console/util/file_manager.dart';
 
 class Game {
   late FileManager fileManager;
@@ -16,6 +16,7 @@ class Game {
   int killedMonster = 0;
 
   Game() {
+    // 게임 시작 전에 파일을 불러와서 사용자와 몬스터 데이터를 가져옵니다.
     fileManager = FileManager();
     character = fileManager.loadCharacter();
     monsters = fileManager.loadMonsterData(character);
@@ -23,15 +24,22 @@ class Game {
 
   void startGame() {
     print('게임을 시작합니다!');
+    // ℹ️ 캐릭터의 현재 정보 표시
     character.showStatus();
 
     while (true) {
+      // 불러온 몬스터중에서 랜덤으로 한마리의 몬스터를 선택
       Monster currentMonster = getRandomMonster();
       print('\n새로운 몬스터가 나타났습니다!');
+      // ℹ️ 몬스터의 현재 정보 표시
       currentMonster.showStatus();
 
+      // ⚠️ 전투 시작 ⚠️
+      // 캐릭터 공격 or 방어
+      // 몬스터 공격
       battle(currentMonster);
 
+      // 싸움 이후의 캐릭터 혹은 몬스터의 체력을 확인하고 게임 종료 여부 판단
       if (character.health <= 0) {
         print('게임 오버! ${character.name}이(가) 쓰러졌습니다.');
         fileManager.saveResult(false, character);
